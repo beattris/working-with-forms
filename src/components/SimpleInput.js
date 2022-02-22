06e9fea8708718b7e6da799c3834dddbbff9f883
Beattris <beatriceebirim@gmail.com>
2022-02-22 03:51:09 +0100

    added state to get user inputs

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,33 @@
+import React, { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === "") {
+      return;
+    }
+
+    console.log(enteredName);
+    setEnteredName("");
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input 
+          type="text" 
+          id="name" 
+          onChange={nameInputChangeHandler} 
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
